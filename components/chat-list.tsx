f0551309b5b5844519653db3fbd492f939f90bce
sinchang <sinchangwen@gmail.com>
2023-07-28 13:01:20 +0800

    Support MD

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -1,5 +1,8 @@
 import { ChatMessage } from '@/components/chat-message'
+import { cn } from '@/lib/utils'
 import { type Message } from 'ai/react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 
 export interface ChatListProps {
   messages: Message[]
@@ -11,7 +14,29 @@ export const ChatList = ({ messages }: ChatListProps) => {
       {messages.map((message, index) => {
         return (
           <ChatMessage role={message.role} key={message.id}>
-            <p>{message.content}</p>
+            <ReactMarkdown
+              // eslint-disable-next-line react/no-children-prop
+              children={message.content}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || '')
+                  const childStr = String(children).replace(/\n$/, '')
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      {...props}
+                      // eslint-disable-next-line react/no-children-prop
+                      children={childStr}
+                      language={match[1]}
+                      PreTag="div"
+                    />
+                  ) : (
+                    <code {...props} className={cn('text-amber-600', className)}>
+                      {children}
+                    </code>
+                  )
+                }
+              }}
+            />
           </ChatMessage>
         )
       })}
